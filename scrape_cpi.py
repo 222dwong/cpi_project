deef3fb470dfd8421f480842d5b633c66dcaaef9
Dylan Wong <dylanwong@DWongs-MacBook-Air.local>
2024-06-09 14:14:41 -0400

    Added libraries

---
+++ scrape_cpi.py
@@ -0,0 +1,10 @@
+import sys
+import requests as rq
+from bs4 import BeautifulSoup as bs
+from time import sleep
+from time import time
+from random import randint
+from warnings import warn
+import json
+import pandas as pd
+
