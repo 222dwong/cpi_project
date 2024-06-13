91675ecf26453a083a9df52b29cb18e625441bce
Dylan Wong <dylanwong@DWongs-MacBook-Air.local>
2024-06-13 12:16:12 -0400

    added all months; 3 months missing data

--- scrape_cpi.py
+++ scrape_cpi.py
@@ -8,8 +8,8 @@ from warnings import warn
 import json
 import pandas as pd
 
-# URL of webpage to scrape (CPI Index Summary)
-url = 'https://web.archive.org/web/20240531084626/https://www.bls.gov/news.release/cpi.t01.htm'
+# URL of webpage to scrape (CPI Index Summary from web.archive.org)
+url = 'https://web.archive.org/web/20200110033226/https://www.bls.gov/news.release/cpi.t01.htm'
 
 # send a GET request to the webpage
 response = rq.get(url)
@@ -20,10 +20,13 @@ if response.status_code == 200:
     soup = bs(response.content, 'html.parser')
 
     # find the table
-    table = soup.find('table')
+    table = soup.find('table', attrs={'class':'regular'})
+
+    # skip tbody to avoid repeating headings
+    tbody = table.find('tbody')
 
     # iterate over each row in the table
-    rows = table.find('tbody').find_all('tr')
+    rows = tbody.find_all('tr')
 
     # empty list storing data
     table_data = []
@@ -35,21 +38,26 @@ if response.status_code == 200:
         if category:
             category_text = category.text.strip()
 
-            # data for each column
+            # data for each column)
             row = [td.text.strip() for td in data]
-
+            
             row.insert(0, category_text)
             table_data.append(row)
 
-    headers = [th.get_text(strip=True) for th in table.find_all('th')]
-    
+    # headers = [th.get_text(strip=True) for th in table.find_all('th')]
+
+    name = soup.find(id = 'bodytext')
+    titles = name.find_all('th', class_ = 'stubhead')
+
+    headers = [header.text for header in titles]
+
     headers = [headers[i] for i in [0, 1, 5, 6, 7, 8, 9, 10, 11, 12]]
 
     # print(headers)
 
     df = pd.DataFrame(table_data[:], columns=headers)
     print(df.to_string())
-    #df.to_csv('february_2024.txt', index=False)
+    # df.to_csv('november_2019.txt', index=False)
 
 else:
     print(f'Failed to retrieve the webpage. Status code: {response.status_code}')    
